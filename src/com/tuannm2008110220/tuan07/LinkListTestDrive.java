8f916d05ccd310f93d06664896ad940e85e02a46
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2021-10-25 00:11:27 +0700

    Up

---
+++ src/com/tuannm2008110220/tuan07/LinkListTestDrive.java
@@ -0,0 +1,14 @@
+package com.tuannm2008110220.tuan07;
+
+public class LinkListTestDrive {
+    public static void main(String[] args) {
+        
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(4);
+        myLinkedList.add(3);
+        myLinkedList.add(8);
+
+        myLinkedList.printf();
+    }
+}
