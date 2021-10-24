bcbc85bc4eea86ee7ba2bcf83bc2973f032d53a7
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2021-10-24 22:26:55 +0700

    Up

---
+++ src/com/tuannm2008110220/tuan03/Baitapthem/CowTestDrive.java
@@ -0,0 +1,10 @@
+package com.tuannm2008110220.tuan03.Baitapthem;
+
+public static void main(String[] args) {
+    Cow cow;
+    cow = new Cow(15, "Tây Ban Nha", "Đen");
+    System.out.println("\nCân nặng: " + cow.weight +" \nGiống: " + cow.breed +" \nMàu: " + cow.color);
+}
+}
+    
+}
