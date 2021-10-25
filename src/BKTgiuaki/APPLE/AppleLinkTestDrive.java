3ef1ead652cdca5aa725f27fbbf7956e06bf700f
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2021-10-25 13:35:25 +0700

    Úp bài 2 kiểm tra giữa kì

---
+++ src/BKTgiuaki/APPLE/AppleLinkTestDrive.java
@@ -0,0 +1,14 @@
+package APPLE;
+
+public class AppleLinkTestDrive {
+    public static void main(String[] args) {
+        appleLinkedList AppleLinkedList = new AppleLinkedList();
+        appleLinkedList.addHead();
+        appleLinkedList.addTail();
+        appleLinkedList.inThongTin();
+        appleLinkedList.tim();
+        appleLinkedList.removeTail();
+        appleLinkedList.inThongTin();
+    }
+    
+}
