c2bde9b438886d4cc44d6d429260b440551f48bb
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2021-11-08 07:13:47 +0700

    Up

--- src/com/tuannm2008110220/tuan08/sapxep.java
+++ src/com/tuannm2008110220/tuan08/sapxep.java
@@ -1,5 +1,9 @@
 public class sapxep {
+
     public static void main(String[] args) {
+        int[] arr = {5,1,12,-5,16,2,12,14};
+        SC(arr);
+        
         public static void selectionSort(int[] arr ){
             for(int i=0;i<arr.length-1;i++)
             {
@@ -14,6 +18,13 @@ public class sapxep {
             arr[index]=arr[i];
             arr[i]=smallerNumber;
         }
+        System.out.println("Mang da duoc sap xep tang dan");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
+
+
     }
     
 }
