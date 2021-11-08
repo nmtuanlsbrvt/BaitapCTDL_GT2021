39c647f77d761b296da373c22e537acde575caa3
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2021-11-08 07:42:44 +0700

    Up

---
+++ src/com/tuannm2008110220/tuan08/QueueNode.java
@@ -0,0 +1,12 @@
+package com.tuannm2008110220.tuan08;
+
+public class QueueNode {
+    String nhaSanxuat, loai;
+    double gia;
+    int namSx;
+    QueueNode next;
+    QueueNode(){
+
+    }
+    QueueNode(String)
+}
